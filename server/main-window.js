badcde51a8ef883b61337fc8be65f4c4d4bff484
Daniel Hammond <viztheman@protonmail.com>
2022-09-03 22:40:12 -0400

    Shifting some files around to keep things tidy. Adding a spiffy new icon.

--- server/main-window.js
+++ server/main-window.js
@@ -6,10 +6,10 @@ module.exports = function MainWindow() {
 		width: 800,
 		height: 600,
 		webPreferences: {
-			preload: path.join(__dirname, 'preload.js')
+			preload: path.join(__dirname, '../preload.js')
 		}
 	});
 
-	mainWindow.loadFile('index.html');
+	mainWindow.loadFile('./views/index.html');
 	return mainWindow;
 };
